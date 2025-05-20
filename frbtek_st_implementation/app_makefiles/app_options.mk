7d3e49b97c78b91a0a2e816e6e46de2f3d53b421
Flo Ronacher <florian.ronacher@posteo.at>
2025-05-20 16:28:17 +0200

    Remove lib related functions

--- frbtek_st_implementation/app_makefiles/app_options.mk
+++ frbtek_st_implementation/app_makefiles/app_options.mk
@@ -17,37 +17,8 @@ BOARD ?= NUCLEO_U073
 # Target radio
 TARGET_RADIO ?= nc
 
-# Region
-REGION ?= EU_868
-
-# Allow fuota (take more RAM, due to read_modify_write feature) and force lbm build with fuota
-ALLOW_FUOTA ?= no
-FUOTA_VERSION ?= 1
-
-# USE LBM Store and forward (take more RAM on STM32L4, due to read_modify_write feature)
-ALLOW_STORE_AND_FORWARD ?= no
-
 #TRACE
-LBM_TRACE ?= yes
 APP_TRACE ?= yes
-
-# Allow relay 
-ALLOW_RELAY_RX ?= no
-ALLOW_RELAY_TX ?= no
-
-#-----------------------------------------------------------------------------
-# LBM options management
-#-----------------------------------------------------------------------------
-
-# CRYPTO Management
-CRYPTO ?= SOFT
-
-# Multistack 
-# Note: if more than one stack is used, more ram will be used for nvm context saving due to read_modify_write feature
-LBM_NB_OF_STACK ?= 1
-
-# Add any lbm build options (ex: LBM_BUILD_OPTIONS ?= LBM_CLASS_B=yes REGION=ALL)
-LBM_BUILD_OPTIONS ?= LBM_CSMA=yes USE_CSMA_BY_DEFAULT=yes
 
 #-----------------------------------------------------------------------------
 # Optimization
